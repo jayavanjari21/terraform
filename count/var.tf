f5bea32399c93ad3e2311356e79fb769d1af914e
jayavanjari21 <jayavanjari2101@gmail.com>
2025-12-15 17:50:01 +0530

    var files added

---
+++ count/var.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  default = "ami-0e7938ad51d883574"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "key_name" {
+ default = "test-key"
+}
