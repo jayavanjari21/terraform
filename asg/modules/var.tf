6d8077475523b386d4bd6e707c2b82aed713dc66
jayavanjari21 <jayavanjari2101@gmail.com>
2025-12-15 16:38:35 +0530

    asg files added

---
+++ asg/modules/var.tf
@@ -0,0 +1,8 @@
+variable "region" {}
+variable "name_prefix" {}
+variable "image_id" {}
+variable "key_name" {}
+variable "vpc_security_group_ids" {}
+variable "resource_type" {}
+variable "vpc_zone_identifier" {}
+variable "value" {}
