883228f808470279e30a8a04fd9856881af44eeb
jayavanjari21 <jayavanjari2101@gmail.com>
2025-12-15 16:41:48 +0530

    rds files added

---
+++ rds/modules/var.tf
@@ -0,0 +1,19 @@
+variable "region" {}
+
+variable "username" {}
+
+variable "password" {}
+
+variable "engine_version" {}
+
+variable "engine" {}
+
+variable "instance_class" {}
+
+variable "db_name" {}
+
+variable "name" {}
+
+variable "subnet_ids" {}
+
+variable "vpc_security_group_ids" {}
