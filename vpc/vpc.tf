45792b76dcb169eabd2c811482b63d1f3bf3d6ba
jayavanjari21 <jayavanjari2101@gmail.com>
2025-12-22 16:54:14 +0530

    Update vpc.tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,13 +1,9 @@
 module "vpc" {
-  source = "./modules/vpc"
-  region = "ap-south-2"
-  name = "jaya"
-  vpc_id = "vpc-0b601470de9f7e96a"
-  public_subnets = [
-    "subnet-00e6319a0a88945ee",
-    "subnet-03fe18304f51a1530",]
+  source = "./modules"
+  region = "us-east-2"
   project                = "cdecb45"
-  vpc_cidr_block         = "172.0.0.0/16"
-  vpc_cidr_pub_subnet    = "172.0.0.0/20"
-  vpc_cidr_pri_subnet    = "172.0.16.0/20"
+  vpc_cidr_block         = "10.0.0.0/16"
+  vpc_cidr_pub_subnet    = "10.0.1.0/24"
+  vpc_cidr_pri_subnet    = "10.0.2.0/24"
 }
+
